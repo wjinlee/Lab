d196eed32a08e6a017d4694b9c4f25d90135dd3a
wjin.lee@gmail.com <wjin.lee@gmail.com>
2017-12-05 15:33:08 +0900

    data structure

--- data_structure.py
+++ data_structure.py
@@ -10,3 +10,13 @@ for item in shoplist:
 print('\nI also have to buy rice.')
 shoplist.append('rice')
 print('My shopping list is now', shoplist)
+
+print('I will sort my list now')
+shoplist.sort()
+print('Sorted shopping list is', shoplist)
+
+print('The first item I will buy is', shoplist[0])
+olditem = shoplist[0]
+del shoplist[0]
+print('I bought the', olditem)
+print('My shopping list is now', shoplist)
