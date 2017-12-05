4b71e77237d9d7ee3955aab2b889b834e8d99806
wjin.lee@gmail.com <wjin.lee@gmail.com>
2017-12-05 15:23:55 +0900

    data structure

---
+++ data_structure.py
@@ -0,0 +1,12 @@
+# This is my shopping list
+shoplist = ['apple', 'mango', 'carrot', 'banana']
+
+print('I have', len(shoplist), 'items to purchase.')
+
+print('These items are:', end=' ')
+for item in shoplist:
+    print(item, end=' ')
+
+print('\nI also have to buy rice.')
+shoplist.append('rice')
+print('My shopping list is now', shoplist)
